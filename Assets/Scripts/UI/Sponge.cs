7755d15512a3910a792fcd1ac4fd074cc685c398
Ilya Pronin <prilan@mail.ru>
2026-03-23 17:48:17 +0400

    Логика спонжика

---
+++ Assets/Scripts/UI/Sponge.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DollMakeup.UI
+{
+    public class Sponge : MonoBehaviour
+    {
+        [SerializeField] private Button SpongeButton;
+
+        private void Start()
+        {
+            SpongeButton.onClick.AddListener(OnSpongeClicked);
+        }
+
+        private void OnSpongeClicked()
+        {
+            AppModel.Instance.OnSpongeClicked();
+        }
+    }
+}
