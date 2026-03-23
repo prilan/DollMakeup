821dac5162cda6ebc1f57a5124fb72ac59528c47
Ilya Pronin <prilan@mail.ru>
2026-03-23 12:34:40 +0400

    Делаю вкладку книги инструментов. Input System

---
+++ Assets/Scripts/UI/ToolBook/ToolBookTab.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DollMakeup.UI.ToolBook
+{
+    public class ToolBookTab : MonoBehaviour
+    {
+        [SerializeField] private Button TabButton;
+        [SerializeField] private Image ActiveImage;
+        [SerializeField] private Image InactiveImage;
+
+        private bool IsActive;
+
+        private void Start()
+        {
+            Debug.Log("ToolBookTab Start");
+            TabButton.onClick.AddListener(OnTabClicked);
+        }
+
+        private void OnDestroy()
+        {
+            
+        }
+
+        public void SetActive(bool isActive)
+        {
+            IsActive = isActive;
+            ActiveImage.gameObject.SetActive(isActive);
+            InactiveImage.gameObject.SetActive(!isActive);
+        }
+        
+        private void OnTabClicked()
+        {
+            Debug.Log("ToolBookTab OnTabClicked");
+            SetActive(!IsActive);
+        }
+    }
+}
