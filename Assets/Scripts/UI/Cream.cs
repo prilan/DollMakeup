32121a85d0f7eeb43b39a9a5effb2ebebf04fd80
Ilya Pronin <prilan@mail.ru>
2026-03-23 14:05:00 +0400

    Делаю крем для лица. Подключил Text Mesh Pro

---
+++ Assets/Scripts/UI/Cream.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DollMakeup.UI
+{
+    public class Cream : MonoBehaviour
+    {
+        [SerializeField] private Button CreamButton;
+        [SerializeField] private Image CreamImage;
+        [SerializeField] private GameObject CreamTool;
+
+        private void Start()
+        {
+            CreamButton.onClick.AddListener(OnCreamClicked);
+        }
+
+        private void OnCreamClicked()
+        {
+            CreamImage.gameObject.SetActive(false);
+            CreamTool.SetActive(true);
+        }
+    }
+}
