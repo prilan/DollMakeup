94515c6228a0a2a43d6de402391746ad502758da
Ilya Pronin <prilan@mail.ru>
2026-03-23 12:34:40 +0400

    Вкладки книги инструментов. Управление вкладками

---
+++ Assets/Scripts/UI/ToolBook/ToolBookTab.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DollMakeup.UI.ToolBook
+{
+    public class ToolBookTab : MonoBehaviour
+    {
+        [SerializeField] private Button TabButton;
+        [SerializeField] private Image ActiveImage;
+        [SerializeField] private Image InactiveImage;
+
+        public Action OnTabActiveChanged = () => { };
+
+        private bool IsActive;
+
+        private void Start()
+        {
+            TabButton.onClick.AddListener(OnTabClicked);
+        }
+
+        public void SetActive(bool isActive)
+        {
+            IsActive = isActive;
+            ActiveImage.gameObject.SetActive(isActive);
+            InactiveImage.gameObject.SetActive(!isActive);
+            
+            if (isActive)
+                OnTabActiveChanged?.Invoke();
+        }
+        
+        private void OnTabClicked()
+        {
+            if (IsActive)
+                return;
+            
+            SetActive(!IsActive);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ToolBook/ToolBookTabs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DollMakeup.UI.ToolBook
+{
+    public class ToolBookTabs : MonoBehaviour
+    {
+
+        [SerializeField] private List<ToolBookTab> Tabs;
+
+        private void Start()
+        {
+            for (var i = 0; i < Tabs.Count; i++)
+            {
+                int index = i;
+                Tabs[i].OnTabActiveChanged += () => OnTabActiveChanged(index);
+            }
+        }
+
+        private void OnTabActiveChanged(int index)
+        {
+            for (var i = 0; i < Tabs.Count; i++)
+            {
+                if (i != index)
+                {
+                    Tabs[i].SetActive(false);
+                }
+            }
+        }
+    }
+}
