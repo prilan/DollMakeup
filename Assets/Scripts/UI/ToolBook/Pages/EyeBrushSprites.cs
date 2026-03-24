ab69cfba88dc93224d300d0fda9a8307895c1234
Ilya Pronin <prilan@mail.ru>
2026-03-24 18:57:55 +0400

    Применение теней для глаз

---
+++ Assets/Scripts/UI/ToolBook/Pages/EyeBrushSprites.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DollMakeup.UI.ToolBook.Pages
+{
+    [Serializable]
+    public class EyeBrushSprites
+    {
+        public List<SpriteRenderer> BrushSprites;
+    }
+}
